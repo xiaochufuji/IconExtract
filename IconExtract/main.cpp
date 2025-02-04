65f0819e1f24c0fedfcdefabad5f8baa78823ca2
xiaochufuji <Qqq1406177488@qq.com>
2025-02-04 22:23:58 +0800

    change: change the dir stuct

--- IconExtract/main.cpp
+++ IconExtract/main.cpp
@@ -8,5 +8,8 @@ int main() {
 	icon.reload(LR"(C:\Program Files\Google\Chrome\Application\chrome.exe)", 256);
 	if (icon.saveAll(L"./img/chrome", xiaochufuji::IconFormat::PNG))
 		std::cout << "success" << std::endl;
+	icon.reload(LR"(D:\visual stduio\Common7\IDE\devenv.exe)", 256);
+	if (icon.saveAll(L"./img/devenv", xiaochufuji::IconFormat::PNG))
+		std::cout << "success" << std::endl;
 	return 0;
 }
